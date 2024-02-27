7a4bae4bb407b959fad9dc4338796aa414e5960c
ilyasguner <ilyas.guner012@gmail.com>
2024-02-27 11:49:10 +0300

    Add project files.

---
+++ YılanOyunu/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace YılanOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnl = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LblPuan = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LblSüre = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(31, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 62);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pnl
+            // 
+            this.pnl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnl.Location = new System.Drawing.Point(31, 101);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(400, 260);
+            this.pnl.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(53, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Puan:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // LblPuan
+            // 
+            this.LblPuan.AutoSize = true;
+            this.LblPuan.BackColor = System.Drawing.Color.Black;
+            this.LblPuan.ForeColor = System.Drawing.Color.White;
+            this.LblPuan.Location = new System.Drawing.Point(86, 30);
+            this.LblPuan.Name = "LblPuan";
+            this.LblPuan.Size = new System.Drawing.Size(13, 13);
+            this.LblPuan.TabIndex = 3;
+            this.LblPuan.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(242, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Süre:";
+            // 
+            // LblSüre
+            // 
+            this.LblSüre.AutoSize = true;
+            this.LblSüre.BackColor = System.Drawing.Color.Black;
+            this.LblSüre.ForeColor = System.Drawing.Color.White;
+            this.LblSüre.Location = new System.Drawing.Point(271, 30);
+            this.LblSüre.Name = "LblSüre";
+            this.LblSüre.Size = new System.Drawing.Size(19, 13);
+            this.LblSüre.TabIndex = 3;
+            this.LblSüre.Text = "60";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(470, 373);
+            this.Controls.Add(this.LblSüre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LblPuan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pnl);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblPuan;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label LblSüre;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ YılanOyunu/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YılanOyunu
+{
+    public partial class Form1 : Form
+    {
+        Label _yilaninkafasi;
+        int _yilanparçasıarasıboşluk = 2;
+        int _yılanparçasısayısı;
+        int _yılanparçasıboyutu = 20;
+        int _yemboyutu = 20;
+        Label _yem;
+        Random _random;
+        HareketYönü _yön;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _random = new Random();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _yılanparçasısayısı = 0;
+            YemOluştur();
+            YeminYeriniDeğiştir();
+            YılanıYerleştir();
+            timer1.Enabled = true;
+        }
+        private void YenidenBaşlat()//methot her çalıştığında oyun başa sarar
+        {
+            this.pnl.Controls.Clear();
+            _yılanparçasısayısı = 0;
+            YemOluştur();
+            YeminYeriniDeğiştir();
+            YılanıYerleştir();
+            timer1.Enabled = true;
+        }
+        private Label YılanParçasıOluştur(int locationx, int locationy)//dışarıdan x ve y koordinatlarını parametre olarak alan ve label döndüren yılanın ilk parçasını oluşturan method
+        {
+            _yılanparçasısayısı++;
+            Label label = new Label()
+            {
+                Name = "yilanParça" + _yılanparçasısayısı,
+                BackColor = Color.Blue,
+                Width = _yılanparçasıboyutu,
+                Height = _yılanparçasıboyutu,
+                Location = new Point(locationx, locationy)
+            };
+
+            this.pnl.Controls.Add(label);//kod ile oluşturduğumuz yılan kafasınız panele ekliyoruz
+
+            return label;
+        }
+
+        private void YılanıYerleştir()//oluşturulan yılan parçasını panelde yerleştiren method
+        {
+            _yilaninkafasi = YılanParçasıOluştur(0, 0);
+            _yilaninkafasi.Text = ":";
+            _yilaninkafasi.TextAlign = ContentAlignment.MiddleCenter;
+            _yilaninkafasi.ForeColor = Color.White;
+            var locationX = (pnl.Width / 2) - (_yilaninkafasi.Width / 2);
+            var locationY = (pnl.Height / 2) - (_yilaninkafasi.Height / 2);
+            _yilaninkafasi.Location = new Point(locationX, locationY);
+
+        }
+
+        private void YemOluştur()//yem oluşturma methodumuz
+        {
+            Label label = new Label()//yemimiz label türünde olacak aşağıda özelliklerini belirtiyoruz
+            {
+                Name = "yem",
+                BackColor = Color.Yellow,
+                Width = _yemboyutu,
+                Height = _yemboyutu,
+
+            };
+            _yem = label;
+            this.pnl.Controls.Add(label);//bu kod ile oluşturduğumuz yeri panel formdaki panel içine ekliyoruz
+        }
+
+        private void YeminYeriniDeğiştir()//yemin yerini rastgele değiştirdiğimiz methodumuz
+        {
+            var locationY = 0;
+            var locationX = 0;
+            bool durum;
+            do
+            {
+                durum = false;
+                locationX = _random.Next(0, pnl.Width - _yemboyutu); //yemimizin koordinatlarını rasgele ayarlıyoruz
+                locationY = _random.Next(0, pnl.Height - _yemboyutu);
+
+                var rect1 = new Rectangle(new Point(locationX, locationY), _yem.Size);//dikdörtgen oluşturuyoruz 
+
+                foreach (Control control in pnl.Controls)//paneli kontrol ettiriyoruz
+                {
+                    if (control is Label && control.Name.Contains("yilanParça"))//eğer control sonucu bir label bulunursa ve bu labelın adında yılanParça geçiyorusa if içine gir
+                    {
+                        var rect2 = new Rectangle(control.Location, control.Size);//bu labelın koordinatlarında bir dikdörtgen oluştur
+
+                        if (rect1.IntersectsWith(rect2))//eğer yemimiz ve yılan parçası kesişirse durum true olacak
+                        {
+                            durum = true;
+                            break;
+                        }
+                    }
+
+                }
+            } while (durum);
+
+            _yem.Location = new Point(locationX, locationY);
+
+
+        }
+
+        private enum HareketYönü
+        {
+            yukarı,   //hareket yönlerimizi belirtiyoruz ve yukarda HareketYönü türünde bir nesne oluşturuyoruz
+            aşağı,
+            sağa,
+            sola
+        }
+
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)//kullanıcı form açıkken bir tuşa basrsa eğer bu method çalışacak
+        {
+            var keycode = e.KeyCode;//klavyeden basılan tuşu burada bir değişkene atıyoruz
+            if (_yön == HareketYönü.sola && keycode == Keys.Right || _yön == HareketYönü.sağa && keycode == Keys.Left || _yön == HareketYönü.aşağı && keycode == Keys.Up|| _yön == HareketYönü.yukarı && keycode == Keys.Down)
+            {
+                return;//eğer yılan bir yöne giderse kullanıcı tam tersi yöne basarsa bu komjut algılanmaycak çünkü yılan kendi üzerinden geriye dönemez
+            }
+
+            switch (keycode)//hangi yöne basarsa switch o yeri bulacak ve _yön değişkenini
+            {
+                case Keys.Up:
+                    _yön = HareketYönü.yukarı;
+                    break;
+                case Keys.Down:
+                    _yön = HareketYönü.aşağı;
+                    break;
+                case Keys.Right:
+                    _yön = HareketYönü.sağa;
+                    break;
+                case Keys.Left:
+                    _yön = HareketYönü.sola;
+                    break;
+
+
+                default:
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)//timer çalışırken her bir tikte çalışır
+        {
+            YılanınKafasınıTakipEt();
+            var locationX = _yilaninkafasi.Location.X;
+            var locationY = _yilaninkafasi.Location.Y;
+            switch (_yön)
+            {
+                case HareketYönü.yukarı:
+                    _yilaninkafasi.Location = new Point(locationX, locationY - (_yilaninkafasi.Width + _yilanparçasıarasıboşluk));
+                    break;
+                case HareketYönü.aşağı:
+                    _yilaninkafasi.Location = new Point(locationX, locationY + (_yilaninkafasi.Width + _yilanparçasıarasıboşluk));
+                    break;
+                case HareketYönü.sağa:
+                    _yilaninkafasi.Location = new Point(locationX + (_yilaninkafasi.Width + _yilanparçasıarasıboşluk), locationY);
+                    break;
+                case HareketYönü.sola:
+                    _yilaninkafasi.Location = new Point(locationX - (_yilaninkafasi.Width + _yilanparçasıarasıboşluk), locationY);
+                    break;
+                default:
+                    break;
+            }
+
+            Yilanyemiyedimi();
+            OyunBittiMİ();
+        }
+
+        private void OyunBittiMİ()
+        {
+            var rect1 = new Rectangle(_yilaninkafasi.Location, _yilaninkafasi.Size);
+            bool oyunbitti = false;
+            foreach (Control controls in pnl.Controls)
+            {
+                if (controls is Label && controls.Name!=_yilaninkafasi.Name)
+                {
+                    var rect2 = new Rectangle(controls.Location, controls.Size);
+                    if (rect1.IntersectsWith(rect2))
+                    {
+                        oyunbitti = true;
+                        break;
+                    }
+                }
+            }
+            if (oyunbitti)
+            {
+                timer1.Enabled = false;
+               DialogResult cevap= MessageBox.Show("Oyun bitti,Tekrar oynamak ister misiniz?","Punınız:"+LblPuan.Text,MessageBoxButtons.YesNo);
+
+                if (cevap==DialogResult.Yes)
+                {
+                    LblPuan.Text = "0";
+                    YenidenBaşlat();
+                }
+                else
+                {
+                    Close();
+                }
+            }
+        }
+
+        private void Yilanyemiyedimi()//yılan yemi yerse çalışacak method
+        {//iki dikdörtgen oluşturduk birisi yılanın kafası diğeri ise yem olcak
+            var rect1 = new Rectangle(_yilaninkafasi.Location, _yilaninkafasi.Size);
+            var rect2 = new Rectangle(_yem.Location, _yem.Size);
+
+            if (rect1.IntersectsWith(rect2))//eğer bu dikdörtgenelr çarpışırsa if içine girer
+            {
+                LblPuan.Text = (Convert.ToInt32(LblPuan.Text) + 10).ToString();//puan artacak
+                YeminYeriniDeğiştir();//yemin yerini değiştiriyoruz
+                YılanParçasıOluştur(-_yemboyutu, -_yemboyutu);//ilk parçanın arkasına yeni bir parça ekliyoruz
+            }
+        }
+
+        private void YılanınKafasınıTakipEt()//yılanın parçalarının birbirini takip etmesi için yazdığımız methot
+        {
+            if (_yılanparçasısayısı <= 1) return;//eğer yılan tek parça veya hiç yok ise bu methottan çıksın
+
+            for (int i = _yılanparçasısayısı; i > 1; i--)//yılanparçası sayısı 1'den büyükse if içine girecek 
+            {
+                var sonparça = (Label)pnl.Controls[i];
+                var sondanöncekiparça = (Label)pnl.Controls[i - 1];
+                sonparça.Location = sondanöncekiparça.Location;    //if sondan başa doğru kontrol edecek
+                                                                   //son parçanın konumunu ondan önceki parçanın konumuna eşitleyecek böylece her parça kendinden önceki parçayı takip edecek  
+            }
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
